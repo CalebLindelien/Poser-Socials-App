3b21f0878da2dcde2dac741fc95ff9362ade0536
Caleb <caleblindelien@gmail.com>
2023-03-02 17:25:25 -0600

    Added Widgets, Profile page and updated packages

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,51 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// State that will be stored in our global state/this data will be accessible throughout our whole application
 const initialState = {
-    mode: "light",
-    user: null,
-    token: null,
-    posts: [],
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
 };
 
 export const authSlice = createSlice({
-    // Represents the auth workflow
-    name: "auth",
-    // Initial state of the global state pretty much calling itself
-    initialState,
-    // Functions that involve modifying the global state
-    reducers: {
+  name: "auth",
+  initialState,
+  reducers: {
     setMode: (state) => {
-        state.mode = state.mode === "light" ? "dark" : "light";
+      state.mode = state.mode === "light" ? "dark" : "light";
     },
     setLogin: (state, action) => {
-        state.user = action.payload.user;
-        state.token = action.payload.token;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
     },
     setLogout: (state) => {
-        state.user = null;
-        state.token = null;
+      state.user = null;
+      state.token = null;
     },
     setFriends: (state, action) => {
-        if (state.user) {
+      if (state.user) {
         state.user.friends = action.payload.friends;
-        } else {
+      } else {
         console.error("user friends non-existent :(");
-        }
+      }
     },
     setPosts: (state, action) => {
-        state.posts = action.payload.posts;
+      state.posts = action.payload.posts;
     },
     setPost: (state, action) => {
-        const updatedPosts = state.posts.map((post) => {
-        // If post id equals the current post id thats being sent into this function, return the post that we changed
+      const updatedPosts = state.posts.map((post) => {
         if (post._id === action.payload.post._id) return action.payload.post;
-        // Else return what we currently have
         return post;
-        });
-        state.posts = updatedPosts;
+      });
+      state.posts = updatedPosts;
     },
-    },
+  },
 });
 
 export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
